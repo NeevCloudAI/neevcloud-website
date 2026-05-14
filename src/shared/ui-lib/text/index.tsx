d17b46c768ddf37d0c8393cc8df9e180d8c600d0
mayank <mayank.verma@neevcloud.com>
2026-05-14 18:22:51 +0530

    add husky for lint staged

--- src/shared/ui-lib/text/index.tsx
+++ src/shared/ui-lib/text/index.tsx
@@ -4,28 +4,9 @@ import { cn } from "@/lib/utils";
 import { textVariants } from "./text-variants";
 
 interface TextProps
-  extends React.HTMLAttributes<HTMLElement>, VariantProps<typeof textVariants> {
+  extends React.HTMLAttributes<HTMLElement>,
+    VariantProps<typeof textVariants> {
   as?: keyof HTMLElementTagNameMap;
-  lineHeight?: "xs" | "sm" | "md" | "lg" | "xl" | "xxl" | "xxxl";
-  textColor?:
-    | "default"
-    | "primary"
-    | "secondary"
-    | "tertiary"
-    | "white"
-    | "black"
-    | "white-50"
-    | "gray-10"
-    | "blue-40"
-    | "gray-60"
-    | "gray-75"
-    | "red-50"
-    | "green-10"
-    | "primary-60"
-    | "gray-90"
-    | "green-50"
-    | "green-100";
-  fontFamily?: "poppins" | "spaceMono";
 }
 
 function Text({

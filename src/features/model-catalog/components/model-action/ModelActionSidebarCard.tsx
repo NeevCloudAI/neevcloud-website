227b3bb41b2c7cbc32308dc9c105b3c13d1f9d4e
mayank <mayank.verma@neevcloud.com>
2026-05-26 14:48:57 +0530

    refactor lint issues

--- src/features/model-catalog/components/model-action/ModelActionSidebarCard.tsx
+++ src/features/model-catalog/components/model-action/ModelActionSidebarCard.tsx
@@ -17,7 +17,7 @@ const ModelActionSidebarCard = ({
         "md:min-w-100 w-full rounded-md border px-3.75 py-2.5 text-left",
         isActive
           ? "border-primary-75 bg-primary-95"
-          : "border-black-35 bg-black-70 hover:border-gray-100"
+          : "border-black-35 bg-black-70 hover:border-gray-100",
       )}
     >
       <Text

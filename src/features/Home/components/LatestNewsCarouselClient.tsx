959756860ae0ff4ed5d4402402a57a45d08b8f97
Sagar Ludhiyani <hi@rango.design>
2026-07-09 13:00:33 +0530

    Homepage 2026: add -webkit-mask-image so the Latest carousel edge-fade renders on Safari/mobile

--- src/features/Home/components/LatestNewsCarouselClient.tsx
+++ src/features/Home/components/LatestNewsCarouselClient.tsx
@@ -5,7 +5,7 @@ import { LATEST_NEWS_ITEMS } from "../data/latest-news.data";
 // card peeks; scroll/drag to advance.
 export default function LatestNewsCarouselClient() {
   return (
-    <ul className="flex list-none snap-x snap-mandatory gap-4 overflow-x-auto pb-1 [&::-webkit-scrollbar]:hidden [mask-image:linear-gradient(to_right,#000_0%,#000_90%,transparent_100%)]">
+    <ul className="flex list-none snap-x snap-mandatory gap-4 overflow-x-auto pb-1 [&::-webkit-scrollbar]:hidden [-webkit-mask-image:linear-gradient(to_right,#000_0%,#000_90%,transparent_100%)] [mask-image:linear-gradient(to_right,#000_0%,#000_90%,transparent_100%)]">
       {LATEST_NEWS_ITEMS.map((item, index) => (
         <li key={item.title} className="snap-start">
           <LatestNewsCard item={item} priority={index < 3} />
